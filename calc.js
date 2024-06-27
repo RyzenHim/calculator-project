4132cab0f3fe15c2df848c7775b5c721f7190581
Himanshu Amulya Upadhyay <90633968+RyzenHim@users.noreply.github.com>
2024-06-28 03:40:43 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,26 @@
+let input = document.querySelector(".input");
+
+let calculate = (number) => {
+  // input.value= input.value+number;
+  input.value += number;
+};
+
+//condition to handle the exception such as multiple operator at same time .
+let result = () => {
+  try {
+    input.value = eval(input.value);
+  } catch (err) {
+    alert("Enter the valid result ");
+  }
+};
+
+function clr() {
+  input.value = " ";
+}
+
+function del() {
+  // let value = input.value;
+  // input.value = value.substr(0, value.length - 1);
+
+  input.value = input.value.slice(0, -1);
+}
