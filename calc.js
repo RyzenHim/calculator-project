b4d7e80875e0edfe3b0bc1b9c66c7efa09e0fa7a
Himanshu Amulya Upadhyay <90633968+RyzenHim@users.noreply.github.com>
2024-06-29 01:48:26 +0530

    mode added

--- calc.js
+++ calc.js
@@ -24,3 +24,16 @@ function del() {
 
   input.value = input.value.slice(0, -1);
 }
+
+//this to toggle dark and light mode;
+
+let themeToggle = document.querySelector("#day");
+themeToggle.addEventListener("click", function () {
+  document.body.classList.toggle("day_mode");
+
+  if (document.body.classList.contains("day_mode")) {
+    themeToggle.src = "assets/light_mode_24dp_FILL0_wght400_GRAD0_opsz24.svg";
+  } else {
+    themeToggle.src = "assets/dark_mode_24dp_FILL0_wght400_GRAD0_opsz24.svg";
+  }
+});
